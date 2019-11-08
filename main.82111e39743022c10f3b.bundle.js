d7e93478291cb79d70926788646abfecdc4cdd56
dehrhard <d.ehrhard@gastromatic.de>
2019-11-08 08:42:37 +0100

    publish: fix GW-9010: month caption KW is now correctly counted
    generated from commit 726f8159a674d6814e2461645c10593f3596e398

---
+++ main.82111e39743022c10f3b.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(368),c(478),n.exports=c(762)},1:function(n,o){},478:function(n,o,c){"use strict";c.r(o);c(479),c(759)}},[[0,1,2]]]);
